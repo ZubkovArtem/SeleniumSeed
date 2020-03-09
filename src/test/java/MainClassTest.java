a08abf5030fe94d65681b9440aa37f8f7f2a043e
ZubkovArtem <61976392+ZubkovArtem@users.noreply.github.com>
2020-03-09 18:00:30 +0200

    Test

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -31,7 +31,6 @@ public class MainClassTest {
         System.out.println("postingStatus1 = " + postingStatus1);
         System.out.println("postingStatus2 = " + postingStatus2);
         Assert.assertEquals(postingStatus1,postingStatus2);
-        //Test
     }
 
     @After
